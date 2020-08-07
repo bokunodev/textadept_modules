f1597ea8f9cad940088289486cf4e77662c749ae
bokunodev <bokunocode@gmail.com>
2020-08-08 06:40:50 +0700

    update Fri 07 Aug 2020 11:40:50 PM UTC

--- modules/go/init.lua
+++ modules/go/init.lua
@@ -2,31 +2,6 @@ local M = {}
 
 M.format_command = 'gofmt'
 
-local PATHSEP = not WIN32 and '/' or '\\'
-local GOPATH = os.getenv('GOPATH')
-local GOFLAGS = '-v -trimpath -ldflags=\'-s -w -buildid=\' -asmflags=-trimpath='..GOPATH..' -gcflags=-trimpath='..GOPATH
-textadept.run.run_commands.go = 'go run '..GOFLAGS..' .'
-textadept.run.compile_commands.go = 'go build '..GOFLAGS..' .'
-textadept.run.error_patterns.go = {'^(.-):(%d+):(%d+): (.+)$'}
-
-function M.run_go_test()
-    local dir = string.gsub(buffer.filename,'[^'..PATHSEP..']+$','')
-    local _,obj = lfs.dir(dir)
-    local each,go_files = '',''
-    each = obj:next()
-    while each ~= nil do
-        if lfs.attributes(each,'mode') == 'file' and string.find(each,'%.go$') then
-            go_files = go_files..string.format(' "%s%s"',dir,each)
-        end
-        each = obj:next()
-    end
-    obj:close()
-    local p = io.popen('cd '..dir..' && go test -v -c -vet=off '..go_files..' 2>&1')
-    local out = p:read('*a')
-    local status = {p:close()}
-    ui.print(out)
-end
-
 events.connect(events.FILE_AFTER_SAVE, function()
     if buffer:get_lexer() ~= 'go' then return end
     local file = buffer.filename
@@ -40,12 +15,15 @@ events.connect(events.FILE_AFTER_SAVE, function()
         ui.print(M.format_command..' not installed!.')
         return
     end
-    local line,col,msg = string.match(out,':(%d-):(%d)-:([^\n]+)')
+    local line,col,msg = string.match(out,'.*:(%d+):(%d+):([^\n]+)')
     line = tonumber(line)
     buffer.annotation_clear_all()
     buffer.annotation_visible = buffer.ANNOTATION_BOXED
     buffer.annotation_text[line] = msg
     buffer.annotation_style[line] = 6
 end)
+
+snippets.go = {
+}
 
 return M
